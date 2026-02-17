da34e0356f5986c2d86c926a45e956a029ba1535
lilellia <pixielf.0@gmail.com>
2026-02-17 13:15:26 -0500

    move CompletedProcess/PipelineError to results.py

--- bombshell/__init__.py
+++ bombshell/__init__.py
@@ -1,4 +1,5 @@
-from .core import CommandChain, CompletedProcess, exec, Pipeline, PipelineError, Process
+from .core import CommandChain, exec, Pipeline, Process
 from .resources import ResourceData
+from .results import CompletedProcess, PipelineError
 
 __all__ = ["Process", "Pipeline", "CompletedProcess", "PipelineError", "CommandChain", "ResourceData", "exec"]
--- bombshell/core.py
+++ bombshell/core.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
-from dataclasses import dataclass, field
 import itertools
 import os
 from os import PathLike
@@ -18,80 +17,11 @@ else:
     from typing_extensions import Self
 
 from .resources import ResourceData
+from .results import CompletedProcess
 from .stream import consume_stream, feed_stream
 from .wait import wait
 
 S = TypeVar("S", str, bytes)
-
-
-class PipelineError(Exception, Generic[S]):
-    def __init__(self, process: CompletedProcess[S]) -> None:
-        msg = f"Pipeline exited with non-zero exit code(s): {process.exit_codes}"
-        super().__init__(msg)
-        self.process: CompletedProcess[S] = process
-
-
-@dataclass(frozen=True, slots=True)
-class CompletedProcess(Generic[S]):
-    args: tuple[tuple[str, ...], ...]
-    command: str
-    exit_codes: list[int]
-    stdout: S
-    stderr: S
-    runtime: float
-    resources: tuple[ResourceData, ...]
-    _timed_out: bool = field(init=True, repr=False)
-
-    @property
-    def exit_code(self) -> int:
-        return self.exit_codes[-1]
-
-    @property
-    def total_resources(self) -> ResourceData:
-        """Return the total resource usage for all processes in the pipeline.
-        On non-Unix systems, only `real_time` can be non-None.
-        """
-        rtime = self.runtime
-
-        all_user_times = [r.user_time for r in self.resources]
-        utime = None if None in all_user_times else sum(cast(list[float], all_user_times))
-
-        all_system_times = [r.system_time for r in self.resources]
-        stime = None if None in all_system_times else sum(cast(list[float], all_system_times))
-
-        all_maxrss = [r.max_resident_set_size for r in self.resources]
-        maxrss = None if None in all_maxrss else max(cast(list[int], all_maxrss))
-
-        return ResourceData(
-            real_time=rtime,
-            user_time=utime,
-            system_time=stime,
-            max_resident_set_size=maxrss,
-        )
-
-    def timed_out(self) -> bool:
-        """Return True if any of the processes in the pipeline timed out."""
-        return self._timed_out
-
-    def check(self, *, strict: bool = False) -> None:
-        """Raise a PipelineError if the pipeline exited with a non-zero exit code.
-
-        :arg strict:
-            If True, raise PipelineError if any process exited with a non-zero exit code.
-            If False, raise PipelineError if the last process exited with a non-zero exit code.
-
-        :return: None
-        :raise PipelineError: If any (strict=True) or the last (strict=False) process exited with a non-zero exit code.
-        """
-        if strict and any(ec != 0 for ec in self.exit_codes):
-            raise PipelineError(self)
-
-        if not strict and self.exit_code != 0:
-            raise PipelineError(self)
-
-    def exit(self) -> None:
-        """Raise SystemExit with the same exit code as the last process in the pipeline."""
-        sys.exit(self.exit_code)
 
 
 @overload
